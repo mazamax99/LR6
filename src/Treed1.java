2decba7872f604972f39bcca4530b2c1c9a8b50c
mazamax99 <ziperziperziperziper@gmail.com>
2018-11-17 11:27:03 +0600

    With LOG

--- src/Treed1.java
+++ src/Treed1.java
@@ -1,2 +1,19 @@
-public class Treed1 {
+import java.util.Date;
+public class Treed1 extends Thread {
+    public  void run1(){
+        boolean cycle=true;
+        Log log=new Log();
+        long time1=new Date().getTime();
+        long time2=time1;
+        while(cycle==true) {
+            log.logger("Thread1 ran");
+           time2=new Date().getTime();
+            if(time2-time1>5000) {
+                this.interrupt();
+                log.logger("Thread1 stopped");
+                break;
+            }
+        }
+
+    }
 }
--- src/Treed2.java
+++ src/Treed2.java
@@ -1,2 +1,19 @@
-public class Treed2 {
+import java.util.Date;
+public class Treed2  extends Thread{
+    public  void run2(){
+        boolean cycle=true;
+        Log log=new Log();
+        long time1=new Date().getTime();
+        while(cycle==true) {
+            log.logger("Thread2 ran");
+            long time2=new Date().getTime();
+            if(time2-time1>5000) {
+                this.interrupt();
+                log.logger("Thread2 stopped");
+                break;
+            }
+        }
+
+
+    }
 }
